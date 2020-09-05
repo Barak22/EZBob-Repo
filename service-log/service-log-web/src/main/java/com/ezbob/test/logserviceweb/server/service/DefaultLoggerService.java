420417047384d1d2c4166ec1924300f31fb558ac
barakm <barakm@wix.com>
2020-09-05 16:13:04 +0300

    Implement the logger

--- service-log/service-log-web/src/main/java/com/ezbob/test/logserviceweb/server/service/DefaultLoggerService.java
+++ service-log/service-log-web/src/main/java/com/ezbob/test/logserviceweb/server/service/DefaultLoggerService.java
@@ -1,9 +1,10 @@
 package com.ezbob.test.logserviceweb.server.service;
 
+import com.ezbob.test.logserviceweb.api.LoggerService;
 import org.springframework.stereotype.Service;
 
 @Service
-public class LoggerService {
+public class DefaultLoggerService implements LoggerService {
 
     public void log(String logInput) {
         System.out.println(logInput);
