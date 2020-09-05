49beca7c35d94c2a7822cc3086bbc0da633a3337
barakm <barakm@wix.com>
2020-09-05 10:03:40 +0300

    Created controller class

---
+++ service-shuffle/src/main/java/com/ezbob/test/serviceshuffle/server/controller/Controller.java
@@ -0,0 +1,30 @@
+package com.ezbob.test.serviceshuffle.server.controller;
+
+import com.ezbob.test.serviceshuffle.server.service.ShuffleService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+class Controller {
+    private ShuffleService service;
+
+
+    public Controller(ShuffleService service) {
+        this.service = service;
+    }
+
+    @PostMapping("/shuffle")
+    public ShuffleResponse generateArray(@RequestBody ShuffleRequest req) {
+        List<Integer> list = service.generateRandomArray(req.getNumberForRandomArray());
+        return new ShuffleResponse(list);
+    }
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello World!";
+    }
+}
