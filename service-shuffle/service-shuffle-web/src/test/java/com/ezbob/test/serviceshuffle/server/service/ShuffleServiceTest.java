fcadfa5ccabab105cd5d3f5aedbd8e1c73ec5755
barakm <barakm@wix.com>
2020-09-05 21:56:02 +0300

    Added another test for invalid input

--- service-shuffle/service-shuffle-web/src/test/java/com/ezbob/test/serviceshuffle/server/service/ShuffleServiceTest.java
+++ service-shuffle/service-shuffle-web/src/test/java/com/ezbob/test/serviceshuffle/server/service/ShuffleServiceTest.java
@@ -1,8 +1,11 @@
 package com.ezbob.test.serviceshuffle.server.service;
 
+import com.ezbob.test.serviceshuffle.api.ShuffleRequest;
+import com.ezbob.test.serviceshuffle.api.ShuffleResponse;
 import org.hamcrest.Description;
 import org.hamcrest.Matcher;
 import org.hamcrest.TypeSafeMatcher;
+import org.hamcrest.core.IsEqual;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
@@ -11,10 +14,12 @@ import java.util.List;
 
 import static com.ezbob.test.serviceshuffle.server.service.HasTheSameElementMatcher.hasTheSameElementsAs;
 import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.mock;
 
 class ShuffleServiceTest {
 
-    ShuffleService shuffleService = new ShuffleService("6000");
+    LoggerClient loggerClient = mock(LoggerClient.class);
+    ShuffleService shuffleService = new ShuffleService(loggerClient);
 
     @Test
     public void randomArrayForNumberOne() {
@@ -46,6 +51,19 @@ class ShuffleServiceTest {
         List<Integer> expected = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
 
         assertThat("Lists are not equal", list, hasTheSameElementsAs(expected));
+    }
+
+    @Test
+    public void shouldReturnInvalidInputResponse() {
+        ShuffleResponse actual = shuffleService.generateRandomArray(new ShuffleRequest(-1));
+        ShuffleResponse expected = ShuffleResponse.invalidResponse();
+
+        assertThat("Lists are not equal", actual, IsEqual.equalTo(expected));
+
+        actual = shuffleService.generateRandomArray(new ShuffleRequest(1001));
+        expected = ShuffleResponse.invalidResponse();
+
+        assertThat("Lists are not equal", actual, IsEqual.equalTo(expected));
     }
 }
 
