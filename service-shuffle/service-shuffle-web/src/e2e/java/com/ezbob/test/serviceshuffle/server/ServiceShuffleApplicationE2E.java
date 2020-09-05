c1e72196148821f68ccb5a2496e9caf324c19e83
barakm <barakm@wix.com>
2020-09-05 16:21:26 +0300

    Refactored the test class

--- service-shuffle/service-shuffle-web/src/e2e/java/com/ezbob/test/serviceshuffle/server/ServiceShuffleApplicationE2E.java
+++ service-shuffle/service-shuffle-web/src/e2e/java/com/ezbob/test/serviceshuffle/server/ServiceShuffleApplicationE2E.java
@@ -1,23 +1,25 @@
 package com.ezbob.test.serviceshuffle.server;
 
+import com.ezbob.test.logserviceweb.testkit.LogServiceTestKit;
 import com.ezbob.test.serviceshuffle.api.ShuffleRequest;
 import com.ezbob.test.serviceshuffle.api.ShuffleResponse;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.hamcrest.Description;
 import org.hamcrest.Matcher;
 import org.hamcrest.TypeSafeMatcher;
-import org.hamcrest.core.IsEqual;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 
 import java.io.IOException;
-import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.util.Arrays;
 
+import static com.ezbob.test.serviceshuffle.factory.HttpRequestFactory.makePostRequestFor;
+import static com.ezbob.test.serviceshuffle.server.TheSameResponseMatcher.isTheSameResponseAs;
 import static org.junit.Assert.assertThat;
 
 
@@ -27,25 +29,32 @@ class ServiceShuffleApplicationE2E {
     @LocalServerPort
     int port;
 
+    @BeforeAll
+    public static void before() {
+        LogServiceTestKit.setPort(6000);
+        LogServiceTestKit.start();
+    }
+
     @Test
     void getRandomArray() throws IOException, InterruptedException {
         ShuffleRequest shuffleRequest = new ShuffleRequest(5);
-        String body = new ObjectMapper().writeValueAsString(shuffleRequest);
 
-        HttpRequest req = HttpRequest.newBuilder()
-                .POST(HttpRequest.BodyPublishers.ofString(body))
-                .header("Content-Type", "application/json")
-                .uri(URI.create("http://localhost:" + port + "/shuffle"))
-                .build();
+        ShuffleResponse actualShuffleResponse = executeRequestWith(shuffleRequest);
+
+        ShuffleResponse expectedShuffleResponse = new ShuffleResponse(Arrays.asList(1, 2, 3, 4, 5));
+
+        assertThat("The responses are not equal",
+                actualShuffleResponse,
+                isTheSameResponseAs(expectedShuffleResponse));
+    }
+
+    private ShuffleResponse executeRequestWith(ShuffleRequest shuffleRequest) throws IOException, InterruptedException {
+        HttpRequest req = makePostRequestFor("http://localhost:" + port + "/shuffle", shuffleRequest);
 
         HttpResponse<String> res = HttpClient.newHttpClient().send(req, HttpResponse.BodyHandlers.ofString());
-        ShuffleResponse shuffleResponse = new ObjectMapper()
+        return new ObjectMapper()
                 .readerFor(ShuffleResponse.class)
                 .readValue(res.body());
-
-        ShuffleResponse expected = new ShuffleResponse(Arrays.asList(1, 2, 3, 4, 5));
-
-        assertThat("The responses are not equal", shuffleResponse, TheSameResponseMatcher.isTheSameResponseAs(expected));
     }
 }
 
