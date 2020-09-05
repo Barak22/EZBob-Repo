0a7ea36fc70ff9ce42b55c5a8522df69aee6850f
barakm <barakm@wix.com>
2020-09-05 16:22:17 +0300

    Renamed method

--- service-shuffle/service-shuffle-web/src/test/java/com/ezbob/test/serviceshuffle/server/service/ShuffleServiceTest.java
+++ service-shuffle/service-shuffle-web/src/test/java/com/ezbob/test/serviceshuffle/server/service/ShuffleServiceTest.java
@@ -14,11 +14,11 @@ import static org.junit.Assert.assertThat;
 
 class ShuffleServiceTest {
 
-    ShuffleService shuffleService = new ShuffleService();
+    ShuffleService shuffleService = new ShuffleService("6000");
 
     @Test
     public void randomArrayForNumberOne() {
-        List<Integer> list = shuffleService.generateRandomArray(1);
+        List<Integer> list = shuffleService.doGeneration(1);
         List<Integer> expected = Collections.singletonList(1);
 
         assertThat("The size of the lists is not equal", list, hasTheSameElementsAs(expected));
@@ -26,7 +26,7 @@ class ShuffleServiceTest {
 
     @Test
     public void randomArrayForNumberTwo() {
-        List<Integer> list = shuffleService.generateRandomArray(2);
+        List<Integer> list = shuffleService.doGeneration(2);
         List<Integer> expected = Arrays.asList(1, 2);
 
         assertThat("The size of the lists is not equal", list, hasTheSameElementsAs(expected));
@@ -34,7 +34,7 @@ class ShuffleServiceTest {
 
     @Test
     public void randomArrayForNumberFive() {
-        List<Integer> list = shuffleService.generateRandomArray(5);
+        List<Integer> list = shuffleService.doGeneration(5);
         List<Integer> expected = Arrays.asList(1, 2, 3, 4, 5);
 
         assertThat("Lists are not equal", list, hasTheSameElementsAs(expected));
@@ -42,7 +42,7 @@ class ShuffleServiceTest {
 
     @Test
     public void randomArrayForNumberTen() {
-        List<Integer> list = shuffleService.generateRandomArray(10);
+        List<Integer> list = shuffleService.doGeneration(10);
         List<Integer> expected = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
 
         assertThat("Lists are not equal", list, hasTheSameElementsAs(expected));
